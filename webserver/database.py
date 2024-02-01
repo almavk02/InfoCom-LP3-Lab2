1777b595f09c62d6697a358707897cb5f5b4eee1
almavk02 <144720277+almavk02@users.noreply.github.com>
2024-02-01 12:08:02 +0100

    Update database.py

--- webserver/database.py
+++ webserver/database.py
@@ -9,7 +9,7 @@ CORS(app)
 
 # change this to connect to your redis server
 # ===============================================
-redis_server = redis.Redis("REDIS_SERVER", decode_responses=True, charset="unicode_escape")
+redis_server = redis.Redis(host = 'localhost', port = '6379', decode_responses=True, charset="unicode_escape")
 # ===============================================
 
 @app.route('/drone', methods=['POST'])
@@ -20,6 +20,15 @@ def drone():
     drone_longitude = drone['longitude']
     drone_latitude = drone['latitude']
     drone_status = drone['status']
+
+    drone_key = f"drone:{droneID}"
+
+    redis_server.hset(drone_key, 'id', droneID)
+    redis_server.hset(drone_key, 'ip', droneIP)
+    redis_server.hset(drone_key, 'longitude', drone_longitude)
+    redis_server.hset(drone_key, 'latitude', drone_latitude)
+    redis_server.hset(drone_key, 'status', drone_status)
+   
     # Get the infomation of the drone in the request, and update the information in Redis database
     # Data that need to be stored in the database: 
     # Drone ID, logitude of the drone, latitude of the drone, drone's IP address, the status of the drone
